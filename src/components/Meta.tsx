5f4791094958e5f1880c6c658196a90276624613
james <huytn@chainosglobal.com>
2024-10-04 20:19:50 +0700

    fix: change Tall to Taal

--- src/components/Meta.tsx
+++ src/components/Meta.tsx
@@ -11,7 +11,7 @@ type MetaProps = {
 export function Meta({ title, description, imageUrl, timestamp }: MetaProps) {
   return (
     <Head>
-      <title>Tall | {title}</title>
+      <title>Taal | {title}</title>
       <meta name="description" content={description} key="description" />
       <meta property="og:title" content={`TallSwap - ${title}`} key="title" />
       <meta property="og:description" content={description} key="ogdescription" />
