6e49427ea3f7c52bc4b54f41434c5ec5eefb6fb3
Sotatek-CanPham <44857670+Sotatek-CanPham@users.noreply.github.com>
2024-10-11 14:29:58 +0700

    fix: update-new-pool

--- src/hooks/app-data-provider/useAppDataProviderTon.tsx
+++ src/hooks/app-data-provider/useAppDataProviderTon.tsx
@@ -87,7 +87,7 @@ export interface PoolContractReservesDataType {
   // stableBorrowIndex: bigint | string | 0 | number;
 }
 
-export const address_pools = 'EQAuY_RtL69muwgGyDT0YqMCLIqn3tewTP0nAD9kQq49vw7P';
+export const address_pools = 'EQC5ZcLDk8fgFfsUqJwmFFh460leInRJLSyxiWPSvWzMA64I';
 export const MAX_ATTEMPTS = 10;
 export const MAX_ATTEMPTS_50 = 50;
 export const GAS_FEE_TON = 0.3;
