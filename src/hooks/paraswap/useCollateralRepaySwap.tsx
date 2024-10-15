8d19b67e1d8f17c7c211b2a7047083e4e39a1e85
Sotatek-CanPham <44857670+Sotatek-CanPham@users.noreply.github.com>
2024-10-15 15:07:15 +0700

    fix: bug-ATTON-474

--- src/hooks/paraswap/useCollateralRepaySwap.tsx
+++ src/hooks/paraswap/useCollateralRepaySwap.tsx
@@ -4,6 +4,7 @@ import { RateOptions } from '@paraswap/sdk/dist/methods/swap/rates';
 import BigNumber from 'bignumber.js';
 import { useCallback, useEffect, useMemo, useState } from 'react';
 
+import { address_pools } from '../app-data-provider/useAppDataProviderTon';
 import {
   convertParaswapErrorMessage,
   fetchExactInRate,
@@ -84,32 +85,27 @@ export const useCollateralRepaySwap = ({
     swapOut.variableBorrowAPY,
   ]);
 
-  const exactInRate = useCallback(() => {
-    if (isConnectNetWorkTon) {
-      const swapOutAmount = valueToBigNumber(swapIn.amount)
-        .multipliedBy(swapIn.priceInUSD)
-        .div(swapOut.priceInUSD);
-      const amountUSD = valueToBigNumber(swapOutAmount).multipliedBy(swapOut.priceInUSD);
+  const exactInRateTON = useCallback(
+    ({ max }: { max: boolean }) => {
+      const amountRepayUSD = max
+        ? valueToBigNumber(swapIn.amount).multipliedBy(swapIn.priceInUSD)
+        : valueToBigNumber(swapOut.amount).multipliedBy(swapOut.priceInUSD);
 
-      const swapInAmount = BigNumber.min(
-        valueToBigNumber(amountUSD).div(swapIn.priceInUSD),
-        valueToBigNumber(swapIn.amount)
-      );
+      const amountCollateralUSD = valueToBigNumber(swapIn.amount).multipliedBy(swapIn.priceInUSD);
+
+      const amountOutPutUSD = BigNumber.min(amountRepayUSD, amountCollateralUSD);
+
+      const swapOutAmount = amountOutPutUSD.div(swapOut.priceInUSD);
+
+      const amountUSD = valueToBigNumber(swapOutAmount).multipliedBy(swapOut.priceInUSD);
+      const swapInAmount = amountOutPutUSD.div(swapIn.priceInUSD);
 
       const amount = normalizeBN(swapOutAmount, swapOut.decimals * -1);
       const srcAmount = normalizeBN(swapInAmount, swapIn.decimals * -1);
 
-      const options: RateOptions = {
-        partner: 'aave',
-      };
-
-      if (max) {
-        options.includeContractMethods = [ContractMethod.buy];
-      }
-
       return {
         blockNumber: 126563785,
-        network: 10,
+        network: -1,
         srcToken: swapIn.underlyingAsset,
         srcDecimals: swapIn.decimals,
         srcAmount: srcAmount.toFixed(0),
@@ -131,7 +127,7 @@ export const useCollateralRepaySwap = ({
                     srcAmount: srcAmount.toFixed(0),
                     destAmount: amount.toFixed(0),
                     percent: 100,
-                    poolAddresses: ['0xd28f71e383e93c570d3edfe82ebbceb35ec6c412'],
+                    poolAddresses: [`${address_pools}`],
                     data: {
                       path: [
                         {
@@ -152,100 +148,44 @@ export const useCollateralRepaySwap = ({
         gasCostUSD: '0.000973',
         gasCost: '218300',
         side: SwapSide.BUY,
-        contractAddress: '0xDEF171Fe48CF0115B1d80b88dc8eAB59176FEe57',
-        tokenTransferProxy: '0x216b4b4ba9f3e719726886d34a177484278bfcae',
+        contractAddress: '',
+        tokenTransferProxy: '',
         contractMethod: 'buy',
         partnerFee: 0,
         srcUSD: amountUSD.toString(),
         destUSD: amountUSD.toString(),
-        partner: 'aave',
+        partner: 'aave-ton',
         maxImpactReached: false,
-        hmac: '7cae78a93aaa7c3a0659f2d0b1a9bba16caad166',
+        hmac: '',
       };
+    },
+    [
+      swapIn.amount,
+      swapIn.decimals,
+      swapIn.priceInUSD,
+      swapIn.underlyingAsset,
+      swapOut.amount,
+      swapOut.decimals,
+      swapOut.priceInUSD,
+      swapOut.underlyingAsset,
+    ]
+  );
+
+  const exactInRate = useCallback(() => {
+    if (isConnectNetWorkTon) {
+      return exactInRateTON({ max: true });
     } else {
       return fetchExactInRate(swapInData, swapOutData, chainId, userAddress);
     }
-  }, [chainId, swapInData, swapOutData, userAddress]);
+  }, [chainId, exactInRateTON, isConnectNetWorkTon, swapInData, swapOutData, userAddress]);
 
   const exactOutRate = useCallback(() => {
     if (isConnectNetWorkTon) {
-      const swapOutAmount = valueToBigNumber(swapOut.amount);
-      const amountUSD = valueToBigNumber(swapOutAmount).multipliedBy(swapOut.priceInUSD);
-      const swapInAmount = BigNumber.min(
-        valueToBigNumber(amountUSD).div(swapIn.priceInUSD),
-        valueToBigNumber(swapIn.amount)
-      );
-
-      const amount = normalizeBN(swapOutAmount, swapOut.decimals * -1);
-      const srcAmount = normalizeBN(swapInAmount, swapIn.decimals * -1);
-
-      const options: RateOptions = {
-        partner: 'aave',
-      };
-
-      if (max) {
-        options.includeContractMethods = [ContractMethod.buy];
-      }
-
-      return {
-        blockNumber: 126563785,
-        network: 10,
-        srcToken: swapIn.underlyingAsset,
-        srcDecimals: swapIn.decimals,
-        srcAmount: srcAmount.toFixed(0),
-        destToken: swapOut.underlyingAsset,
-        destDecimals: swapOut.decimals,
-        destAmount: amount.toFixed(0),
-        bestRoute: [
-          {
-            percent: 100,
-            swaps: [
-              {
-                srcToken: swapIn.underlyingAsset,
-                srcDecimals: swapIn.decimals,
-                destToken: swapOut.underlyingAsset,
-                destDecimals: swapOut.decimals,
-                swapExchanges: [
-                  {
-                    exchange: 'UniswapV3',
-                    srcAmount: srcAmount.toFixed(0),
-                    destAmount: amount.toFixed(0),
-                    percent: 100,
-                    poolAddresses: ['0xd28f71e383e93c570d3edfe82ebbceb35ec6c412'],
-                    data: {
-                      path: [
-                        {
-                          tokenIn: swapIn.underlyingAsset,
-                          tokenOut: swapOut.underlyingAsset,
-                          fee: '100',
-                          currentFee: '100',
-                        },
-                      ],
-                      gasUSD: '0.000486',
-                    },
-                  },
-                ],
-              },
-            ],
-          },
-        ],
-        gasCostUSD: '0.000973',
-        gasCost: '218300',
-        side: SwapSide.BUY,
-        contractAddress: '0xDEF171Fe48CF0115B1d80b88dc8eAB59176FEe57',
-        tokenTransferProxy: '0x216b4b4ba9f3e719726886d34a177484278bfcae',
-        contractMethod: 'buy',
-        partnerFee: 0,
-        srcUSD: amountUSD.toString(),
-        destUSD: amountUSD.toString(),
-        partner: 'aave',
-        maxImpactReached: false,
-        hmac: '7cae78a93aaa7c3a0659f2d0b1a9bba16caad166',
-      };
+      return exactInRateTON({ max: false });
     } else {
       return fetchExactOutRate(swapInData, swapOutData, chainId, userAddress, max);
     }
-  }, [chainId, max, swapInData, swapOutData, userAddress]);
+  }, [chainId, exactInRateTON, isConnectNetWorkTon, max, swapInData, swapOutData, userAddress]);
 
   useEffect(() => {
     if (skip) return;
