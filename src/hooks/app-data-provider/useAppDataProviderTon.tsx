4257e1760014f01717d5d1a852990e98f3545728
Sotatek-CanPham <44857670+Sotatek-CanPham@users.noreply.github.com>
2024-11-05 09:24:54 +0700

    fix: update-new-pool

--- src/hooks/app-data-provider/useAppDataProviderTon.tsx
+++ src/hooks/app-data-provider/useAppDataProviderTon.tsx
@@ -89,7 +89,7 @@ export interface PoolContractReservesDataType {
   // stableBorrowIndex: bigint | string | 0 | number;
 }
 export const FACTORY_DEDUST_TESTNET = 'EQAROb_l-1yGMKjPGUmc0tNjYOsXTKTsucXmhh2Fm9y98z7Y';
-export const address_pools = 'EQB1hX12w4ani36_a8uDBJpWsEhV0qpNSnHHj9Kdkkt-kHR3';
+export const address_pools = 'EQBar4i236ZF0yBYJaeJFtW292Y8opGM7Euci3j7avEhwzLF';
 export const MAX_ATTEMPTS = 10;
 export const MAX_ATTEMPTS_50 = 50;
 export const GAS_FEE_TON = 0.3;
@@ -719,7 +719,7 @@ export const useAppDataProviderTon = (ExchangeRateListUSD: WalletBalanceUSD[]) =
       };
     });
     if (JSON.stringify(newReserves) !== JSON.stringify(reservesTon)) {
-      console.log('Assets to supply---------------', newReserves);
+      console.log('Assets to supply---------------', address_pools, newReserves);
       setReservesTon(newReserves);
       sleep(2000);
       setLoading(false);
