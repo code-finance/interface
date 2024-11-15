dbf007ec405b6178ac18113d8394069644ad8687
Sotatek-CanPham <44857670+Sotatek-CanPham@users.noreply.github.com>
2024-11-15 16:07:07 +0700

    fix: update-new-pool

--- src/hooks/app-data-provider/useAppDataProviderTon.tsx
+++ src/hooks/app-data-provider/useAppDataProviderTon.tsx
@@ -90,7 +90,7 @@ export interface PoolContractReservesDataType {
 }
 
 export const FACTORY_DEDUST_TESTNET = 'EQAROb_l-1yGMKjPGUmc0tNjYOsXTKTsucXmhh2Fm9y98z7Y';
-export const address_pools = 'EQA8VDeNIokPAyMur3lJVkLwegYuN81Q4I9ZOn98UtUrTaMH';
+export const address_pools = 'EQAOg61c_JPLsPeB9fpzDWju1fkzXOXjSkRndxOZvdxDSeDE';
 export const MAX_ATTEMPTS = 100;
 export const MAX_ATTEMPTS_50 = 50;
 export const GAS_FEE_TON = 0.3;
