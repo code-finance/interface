df73af421c34e2b4b17e2c084e3fe6a3fc538464
Sotatek-CanPham <44857670+Sotatek-CanPham@users.noreply.github.com>
2024-12-09 14:59:01 +0700

    fix: update-gass-sc

--- src/sc-contracts/app/index.ts
+++ src/sc-contracts/app/index.ts
@@ -114,7 +114,7 @@ export class App {
     const wallet = this.wallet(await minter.getWalletAddress(via.address));
 
     // TODO: Move the following constants to external files
-    const SUPPLY_MESSAGE_VALUE = toNano(0.2);
+    const SUPPLY_MESSAGE_VALUE = toNano(0.25);
     const SUPPLY_MESSAGE_OP = 0x1530f236;
     const FORWARD_TON_AMOUNT = toNano(0.1);
     const FORWARD_PAYLOAD = beginCell().storeUint(SUPPLY_MESSAGE_OP, 32).endCell();
